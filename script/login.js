b2ef3b4e21af08d455fbee747cfa428ae64c7023
Amit Kumar <amitmishra74880@gmail.com>
2022-10-12 13:31:32 +0530

    front

---
+++ script/login.js
@@ -0,0 +1,6 @@
+import navbar from "../components/navbar.js"
+import footer from "../components/footer.js"
+
+document.querySelector("#login-signup-navbar").innerHTML=navbar();
+document.querySelector("#login-footer").innerHTML=footer();
+
---
+++ script/signup.js
@@ -0,0 +1,7 @@
+import navbar from "../components/navbar.js"
+import footer from "../components/footer.js"
+
+document.querySelector("#login-signup-navbar").innerHTML=navbar();
+document.querySelector("#login-footer").innerHTML=footer();
+
+
