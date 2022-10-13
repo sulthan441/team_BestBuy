317a707b3dc0bd88ee6bb58ede3bc0d387daf913
Amit Kumar <amitmishra74880@gmail.com>
2022-10-13 16:01:00 +0530

    final

--- script/login.js
+++ script/login.js
@@ -4,3 +4,30 @@ import footer from "../components/footer.js"
 document.querySelector("#login-signup-navbar").innerHTML=navbar();
 document.querySelector("#login-footer").innerHTML=footer();
 
+let data=JSON.parse(localStorage.getItem('user')) || [];
+// console.log(data);
+let flag=0;
+document.querySelector(".login-btn").addEventListener('click',()=>{
+    console.log(data);
+    let c=check(data);
+    if(flag===1){
+        alert("Login Successfully");
+        location.href="./index.html"
+    }
+    else{
+        alert("Invalid Email or Create an account");
+    }
+})
+
+const check=(data)=>{
+    let email=document.querySelector(".email").value;
+    let password=document.querySelector(".password").value;
+    data.forEach(el => {
+        if(email===el.email && password===el.password){
+            console.log("checked")
+            flag=1;
+            return true
+        }
+    });
+}
+
